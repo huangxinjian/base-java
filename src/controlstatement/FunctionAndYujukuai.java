1bec4da07d602ec9f3d0b00e89646cdf124ecef9
huangxinjian <924168702@qq.com>
2019-01-27 16:54:35 +0800

    语句块和方法

---
+++ src/controlstatement/FunctionAndYujukuai.java
@@ -0,0 +1,20 @@
+package controlstatement;
+
+/**
+ * @Author: huangxinjian
+ * @Description: 语句块和方法
+ * @Date: Created in 16:04 2019/1/27
+ * @Modified By:
+ */
+public class FunctionAndYujukuai {
+
+    // 方法就是带有名称的语句块 main就是一个方法
+    public static void main(String[] args) {
+
+        { //这就是语句块
+            int a = 10;
+        }
+
+
+    }
+}
