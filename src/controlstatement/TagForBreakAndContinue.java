95cf3b9fecd9baff1b4d291a9ed67516d9af5ef2
huangxinjian <924168702@qq.com>
2019-01-27 16:54:23 +0800

    带标签的break和Continue

--- src/controlstatement/TagForBreakAndContinue.java
+++ src/controlstatement/TagForBreakAndContinue.java
@@ -7,4 +7,15 @@ package controlstatement;
  * @Modified By:
  */
 public class TagForBreakAndContinue {
+
+    public static void main(String[] args) {
+        outer: for (int i = 101; i < 150; i++) {
+            for (int j = 2; j < i / 2; j++) {
+                if (i % j == 0){
+                    continue outer; //跳转到outer代码所在的地方
+                }
+            }
+            System.out.print(i + "  ");
+        }
+    }
 }
