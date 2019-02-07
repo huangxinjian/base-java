c60fed9d7b909bb484967ec5cd7df40e398b0fa8
huangxinjian <924168702@qq.com>
2019-02-07 11:27:05 +0800

    final关键字

---
+++ src/object_oriented2/TestFinal.java
@@ -0,0 +1,25 @@
+package object_oriented2;
+
+/**
+ * @Author: huangxinjian
+ * @Description:  测试 final 关键字
+ * @Date: Created in 11:19 2019/2/7
+ * @Modified By:
+ */
+public class TestFinal {
+
+    /*
+    final 可以用来修饰 类、变量、方法
+
+         成员变量： final 修饰成员变量，不需要立即初始化，但必须在构造函数中进行初始化
+         局部变量： 需要立即初始化才能使用
+          被final修饰的变量称为常量，一经初始化，无法修改
+
+          final修饰的方法不能被重写，但是可以被重载
+
+          final修饰的类不能被继承
+     */
+    public static void main(String[] args) {
+
+    }
+}
