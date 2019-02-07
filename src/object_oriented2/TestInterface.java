021c62594728c179182ebda3053fe580ce39ffb4
huangxinjian <924168702@qq.com>
2019-02-07 12:36:45 +0800

    接口

---
+++ src/object_oriented2/TestInterface.java
@@ -0,0 +1,13 @@
+package object_oriented2;
+
+/**
+ * @Author: huangxinjian
+ * @Description:  interface 接口
+ * @Date: Created in 12:24 2019/2/7
+ * @Modified By:
+ */
+public interface TestInterface {
+
+    /* 默认前缀： public static final */ int MAX_INT = 10;
+    /* 默认前缀： public abstract */ void text01();
+}
