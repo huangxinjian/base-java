4876ac0712906466b6d6f6e5906a9ed459b36a41
huangxinjian <924168702@qq.com>
2019-02-13 16:20:37 +0800

    包装类的种类

---
+++ src/commonly_used_class/PackagingClassSort.java
@@ -0,0 +1,20 @@
+package commonly_used_class;
+
+/**
+ * @Author: huangxinjian
+ * @Description: 包装类
+ * @Date: Created in 12:18 2019/2/8
+ * @Modified By:
+ */
+public class PackagingClassSort {
+    /*
+    每个基本数据类型都对应了一种包装类：
+            byte        Byte
+            short       Short
+            int         Integer
+            long        Long
+            char        Character
+            float       Float
+            double      Double
+     */
+}
