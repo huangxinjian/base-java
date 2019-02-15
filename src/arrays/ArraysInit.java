a034cd3d43ba3cb7af8c8eb62e9f17cd8f97e984
huangxinjian <924168702@qq.com>
2019-02-15 22:05:59 +0800

    数组初始化方式

--- src/arrays/ArraysInit.java
+++ src/arrays/ArraysInit.java
@@ -6,7 +6,7 @@ package arrays;
  * @Date: Created in 11:31 2019/2/7
  * @Modified By:
  */
-public class Test01 {
+public class ArraysInit {
 
     /*
     数组其实也是对象，数组中的值可以看作是该对象的成员变量，数组的值都存储在 堆 中
@@ -15,5 +15,13 @@ public class Test01 {
     public static void main(String[] args) {
        int[] a = new int[3];  //声明一个 int数组类型的变量a
        int b[] = new int[3];  //声明一个 int类型的数组变量b
+
+       //int [] c = new int[3]{1,2,3};  //这样是错误的，不能动态 静态初始化同时进行
+       int[] c = {1,2,3};  //静态初始化
+       int[] d= new int[3];  //默认初始化
+
+        int[] a1 = new int[2];//动态初始化数组，先分配空间；
+        a1[0]=1;//给数组元素赋值；
+        a1[1]=2;//给数组元素赋值；
     }
 }
---
+++ src/container/ContainerIntroduce.java
@@ -0,0 +1,40 @@
+package container;
+
+/**
+ * @Author: huangxinjian
+ * @Description:  容器的介绍
+ * @Date: Created in 20:16 2019/2/15
+ * @Modified By:
+ */
+public class ContainerIntroduce {
+
+    /*
+        什么是容器？
+            容器就是一个用来装对象的对象，
+
+
+         就像数组，数组本身也是一种容器，可以在里面存放一些基本数据类型和引用类型
+            数组的优势:  1. 数组是一种简单的线性序列，可以快速访问数组元素，效率高
+
+            数组的缺点： 2. 不灵活，我们在使用数组的时候，一开始就要指定数组的大小，例如 int[] a= new int[3];
+                            不能随着数据的变大而扩容
+     */
+
+
+    /*
+    在 Java 中，容器分为下面两大分类：
+
+                           <<interface>>                       <<interface>>
+                            Collection                              Map
+                         /               \                           |
+                 <<interface>>      <<interface>>                 HashMap
+                      Set                List
+                       |                /    \
+                    HashSet       ArrayList  LinkedList
+
+
+            其中 set 集合 和 List 集合的区别就在于：
+                1. Set 集合是无序的，且不可存在重复数据
+                2. List 集合是有序的，且可以存在重复数据
+     */
+}
