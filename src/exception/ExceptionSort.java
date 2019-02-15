c23b72300fc658b0e89d0904ae7ae48d733a9d99
huangxinjian <924168702@qq.com>
2019-02-15 20:13:29 +0800

    异常的分类

---
+++ src/exception/ExceptionSort.java
@@ -0,0 +1,25 @@
+package exception;
+
+/**
+ * @Author: huangxinjian
+ * @Description: 异常的分类
+ * @Date: Created in 15:15 2019/2/15
+ * @Modified By:
+ */
+
+public class ExceptionSort {
+
+    /*
+    异常一共分为两大类
+            1. 运行时异常   RuntimeException
+            2. 检查时异常   CheckedException
+
+
+            所有的异常的根类为                java.lang.Throwable
+                                             /                  \
+            被两大子类继承           Error（我们不需要管）   Exception(我们常用这个)
+                                                              /               \
+                                       (编译器会处理)CheckedException      RuntimeException
+
+     */
+}
