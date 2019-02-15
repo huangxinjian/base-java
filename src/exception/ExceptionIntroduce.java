50b0a61296ef0f0808b1ce702cd87e2ac018d387
huangxinjian <924168702@qq.com>
2019-02-15 20:13:22 +0800

    异常介绍

---
+++ src/exception/ExceptionIntroduce.java
@@ -0,0 +1,20 @@
+package exception;
+
+/**
+ * @Author: huangxinjian
+ * @Description:  什么是异常?
+ * @Date: Created in 15:07 2019/2/15
+ * @Modified By:
+ */
+public class ExceptionIntroduce {
+
+    /*
+      异常是 Java 在发生错误情况的时候的一种处理机制，通常用以下格式来实现
+            try{
+                //我们的业务代码
+            }catch{
+                //异常捕获处理的地方
+            }
+
+     */
+}
