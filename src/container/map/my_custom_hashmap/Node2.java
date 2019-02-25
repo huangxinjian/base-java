e84130af53f60b034d2de408509edc03a86cfed6
huangxinjian <924168702@qq.com>
2019-02-25 11:42:02 +0800

    Node2非泛型节点

---
+++ src/container/map/my_custom_hashmap/Node2.java
@@ -0,0 +1,15 @@
+package container.map.my_custom_hashmap;
+
+/**
+ * @Author: huangxinjian
+ * @Description:  HashMap的节点
+ * @Date: Created in 21:33 2019/2/23
+ * @Modified By:
+ */
+public class Node2 {
+
+    int hash;  //hash值
+    Object key;  // key
+    Object value;   //value
+    Node2 next;  //下一个节点
+}
