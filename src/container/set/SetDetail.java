c8b2cf534333bf7f7c9d64344069311d5a5fdd03
huangxinjian <924168702@qq.com>
2019-02-25 14:16:33 +0800

    Set集合详解

---
+++ src/container/set/SetDetail.java
@@ -0,0 +1,20 @@
+package container.set;
+
+/**
+ * @Author: huangxinjian
+ * @Description: Set详解
+ * @Date: Created in 11:44 2019/2/25
+ * @Modified By:
+ */
+public class SetDetail {
+
+    /*
+            Set容器： 无序、不可重复
+
+              无序指的是set中的元素没有索引,我们只能遍历查找
+              不可重复指不允许加入重复的元素
+
+              新元素如果和Set中某个元素通过equals()方法对比为true，则不能加入;
+              甚至，Set中也只能放入一个null元素，不能多个。
+     */
+}
